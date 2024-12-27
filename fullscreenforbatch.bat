7c49f49165adce9b53b0aebb20aa2d4bc4afc79c
XdragonianX <158476287+XdragonianX@users.noreply.github.com>
2024-12-27 19:46:15 +0200

    Update and rename main.bat to fullscreenforbatch.bat

---
+++ fullscreenforbatch.bat
@@ -0,0 +1,8 @@
+:VBSDynamicBuild
+SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
+IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
+ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+CSCRIPT //nologo "%TempVBSFile%"
